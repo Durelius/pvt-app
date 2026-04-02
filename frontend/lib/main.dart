f043601ef552f935098a9b4ca7cb35c7ea26d285
kimotheus <k.lindfors92@gmail.com>
2026-04-02 15:09:45 +0200

    green color scheme

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: const Color(0xFF99D98C)), //#99D98C
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
