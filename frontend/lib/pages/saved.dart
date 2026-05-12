53f697fe30dca3b5bd66fa6b20e85938a3d2aea5
RobinCenterlind <centerlind.robin@gmail.com>
2026-05-12 14:21:24 +0200

    added header to the saved page

--- frontend/lib/pages/saved.dart
+++ frontend/lib/pages/saved.dart
@@ -13,19 +13,45 @@ class _SavedPageState extends State<SavedPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Color(0xFFF5F5F5),
-      body: Center(
-        child: Text(
-          'Saved',
-          style: TextStyle(
-            color: kPurple,
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
+      body: Column(
+        children: [
+          _buildHeader(),
+        ],
       ),
     );
   }
+
+  
+Widget _buildHeader() {
+  return Padding(
+    padding: const EdgeInsets.fromLTRB(16, 35, 16, 16),
+    child: Stack(
+      alignment: Alignment.center,
+      children: [
+        const Center(
+          child: Text(
+            "Saved",
+            style: TextStyle(
+              color: Color(0xFF63519F),
+              fontSize: 26,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        const Align(
+          alignment: Alignment.centerRight,
+          child: Icon(
+            Icons.bookmark_rounded,
+            color: Color(0xFF63519F),
+            size: 32,
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
 
 }
