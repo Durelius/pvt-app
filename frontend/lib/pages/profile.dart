706364a4e90bf08750f74742a5229a3237f36576
michaelflodqvist <michaelflodqvist03@gmail.com>
2026-05-20 03:31:01 +0200

    Update profile.dart

--- frontend/lib/pages/profile.dart
+++ frontend/lib/pages/profile.dart
@@ -12,6 +12,7 @@ class _ProfilePageState extends State<ProfilePage> {
     final FocusNode _focusNode = FocusNode();
     final TextEditingController _textFieldController = TextEditingController();
     final homeAddressBox = Hive.box('homeAddress');
+    Color _fillColor = Color(0xFFE8DEF8);
 
     @override
     void initState() {
@@ -24,18 +25,22 @@ class _ProfilePageState extends State<ProfilePage> {
       }
 
       _focusNode.addListener(() {
+        setState(() {
+          _fillColor = _focusNode.hasFocus ? Color(0xFFF4EFF6) : Color(0xFFE8DEF8);
+        });
+
         if(!_focusNode.hasFocus) {
             homeAddressBox.put('homeAddress', _textFieldController.text);
         }
       });
     }
 
-  @override
-  void dispose() {
-    _focusNode.dispose();
-    _textFieldController.dispose();
-    super.dispose();
-  }
+    @override
+    void dispose() {
+      _focusNode.dispose();
+      _textFieldController.dispose();
+      super.dispose();
+    }
 
     @override
     Widget build(BuildContext context) {
@@ -67,12 +72,22 @@ class _ProfilePageState extends State<ProfilePage> {
                         focusNode: _focusNode,
                         controller: _textFieldController,
                         decoration: InputDecoration(
-                            border: OutlineInputBorder(
+                            filled: true,
+                            fillColor: _fillColor,
+                            enabledBorder: OutlineInputBorder(
                                 borderSide: BorderSide(
-                                    color: Color(0xFF6750A4)
+                                    color: Color(0xFFE8DEF8),
+                                    width: 2
                                 )
                             ),
-                            hintText: 'Your Address Here'
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color(0xFF6750A4),
+                                    width: 2
+                                )
+                            ),
+                            hintText: 'Your Address Here',
+                            hintStyle: TextStyle(color: Color(0xFF6750A4))
                         )
                       )
                     )
