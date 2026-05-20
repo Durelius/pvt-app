49e16f2575f8f94376ac8c684ddfe46f73c0b54c
kimotheus <k.lindfors92@gmail.com>
2026-05-20 13:54:45 +0200

    Update splash.dart

--- frontend/lib/pages/splash.dart
+++ frontend/lib/pages/splash.dart
@@ -56,7 +56,7 @@ class MittenLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Image.asset(
-      'assets/logo.png',
+      'assets/mitten_logo.png',
       width: size,
       height: size,
       fit: BoxFit.contain,
