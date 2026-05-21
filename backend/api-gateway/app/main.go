aa3237e41e114ac16017185f58fdc8a4a3653002
Durelius <wilhelm@durelius.se>
2026-05-21 20:18:26 +0200

    fix: cors fix for prod

--- backend/api-gateway/app/main.go
+++ backend/api-gateway/app/main.go
@@ -15,6 +15,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func proxyTo(target string) http.HandlerFunc {
 	URL, _ := url.Parse(target)
 	proxy := httputil.NewSingleHostReverseProxy(URL)
@@ -31,6 +44,7 @@ func main() {
 	}
 
 	r := mux.NewRouter()
+	r.Use(corsMiddleware)
 
 	entries, err := os.ReadDir("../services/")
 	if err != nil {
