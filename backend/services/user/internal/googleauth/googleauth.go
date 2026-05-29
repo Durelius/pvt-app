87682cf1f5b67bfcee233444492c6c2feebe66c9
Kizmorak <erik-94@live.se>
2026-05-29 16:56:57 +0200

    Added new client IDs to the list of valid Google OAuth client IDs in the googleauth.go file. Also updated the google-services.json file in the Android app to include the new client IDs for proper authentication.

--- backend/services/user/internal/googleauth/googleauth.go
+++ backend/services/user/internal/googleauth/googleauth.go
@@ -12,6 +12,10 @@ import (
 var validClientIDs = []string{
 	"648166383994-cjdcvd4s66l8uuf84nn7gs2lqh1r1jva.apps.googleusercontent.com",
 	"169231317250-nc8otuvk6ic7cqii3sfdd4pbbp8ge9d7.apps.googleusercontent.com",
+	"648166383994-2s5edahe0h27fqrjp822un6ki6cneun4.apps.googleusercontent.com",
+	"648166383994-60injkuta3jv8thamge46fevoq4kjd3v.apps.googleusercontent.com",
+	"648166383994-8l68bnq765b15egumi92q9o0q7hagq4q.apps.googleusercontent.com",
+	"648166383994-cjdcvd4s66l8uuf84nn7gs2lqh1r1jva.apps.googleusercontent.com",
 }
 
 type GoogleProfile struct {
